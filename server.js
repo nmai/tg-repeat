21f049e6c07d3a9dc22585940dd1b6c70f10d130
Nick Mai <nick.mai@outlook.com>
2015-12-15 20:34:12 -0800

    npm module with deps

---
+++ server.js
@@ -0,0 +1,8 @@
+/** 
+ * Nick Mai ~ Dec. 2015
+ * 
+ * Demo server for testing the tg-repeat angular module
+ */
+
+ var express = require('express') 
+
---
+++ tg-repeat.js
@@ -0,0 +1,7 @@
+/**
+ * Nick Mai ~ Dec. 2015
+ * 
+ * tgRepeat: an optimized version of ngRepeat intended for
+ *           specific use cases.
+ */
+
